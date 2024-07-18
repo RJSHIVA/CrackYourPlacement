e50fe4a966c90b2d5fa6823e5dc1139176b242da
RAHUL CHOUDHARY <rc725383@gmail.com>
2024-07-18 19:47:12 +0530

    done

---
+++ ARRAY_MEDIUM/largest_rectangle.java
@@ -0,0 +1,31 @@
+import java.util.Stack;
+
+public class Solution {
+
+    public static int largestRectangleArea(int[] heights) {
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+        int index = 0;
+
+        while (index < heights.length) {
+            if (stack.isEmpty() || heights[index] >= heights[stack.peek()]) {
+                stack.push(index);
+                index++;
+            } else {
+                int topOfStack = stack.pop();
+                int area = heights[topOfStack] * (stack.isEmpty() ? index : index - stack.peek() - 1);
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        while (!stack.isEmpty()) {
+            int topOfStack = stack.pop();
+            int area = heights[topOfStack] * (stack.isEmpty() ? index : index - stack.peek() - 1);
+            maxArea = Math.max(maxArea, area);
+        }
+
+        return maxArea;
+    }
+
+
+}
